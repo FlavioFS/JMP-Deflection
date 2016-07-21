cde1b23e92a16519abebd5a18c4919a0dadb109a
falquinho <lucas.falcao.nb@gmail.com>
2016-07-21 08:43:36 -0300

    limpeza dos arquivos do projeto

--- Deflection/assets/sprites/generate_and_move_wizards.bat
+++
@@ -1,7 +0,0 @@
-START "rescomp" "rescomp.exe" wizard_res.txt
-
-pause
-
-move wizard_res.h ..\..\inc\sprites
-
-move wizard_res.s ..\..\src\
--- Deflection/assets/tilesets/generate_and_move_tileset.bat
+++
@@ -1,7 +0,0 @@
-START "rescomp" "rescomp.exe" arena1_tileset.txt
-
-pause
-
-move arena1_tileset.h ..\..\inc\tilesets
-
-move arena1_tileset.s ..\..\src\
